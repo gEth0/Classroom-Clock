f0c59bf6296614a0fc25d55d75df215ebfff60cb
gEth0 <solo.per.testing.py@gmail.com>
2022-10-07 20:34:49 +0200

    Fixing bugs

--- ClassClock.sh
+++
@@ -1,1 +0,0 @@
-python tkLess.py
--- tkLess.py
+++ tkLess.py
@@ -3,15 +3,16 @@ import time
 import webbrowser
 import selenium
 from selenium import webdriver
+
 f = Tk()
 f.config(bg="grey")
 f.geometry("550x400")
 f.title("ClassroomClock")
 f.resizable(0, 0)
-icon = PhotoImage(file="google-meet.png")
+icon = PhotoImage(file="./img/google-meet.png")
 f.tk.call("wm", "iconphoto", f._w, icon)
 
-
+# list of classes, insert here your classes's links
 ita = ""
 mate = ""
 spa = ""
@@ -27,13 +28,13 @@ ed = ""
 time1 = time.strftime("%H:%M")
 saluto = ''
 if time1 > '13:00':
-    saluto = 'BuonPomeriggio'
+    saluto = 'Good Afternoon'
 else:
-    saluto = 'Buongiorno'
+    saluto = 'Good Morning'
 
 Label(f, text=saluto, height="3", width="500", font=(
     "Francisco", 15, 'bold'), bg="grey").pack()
-Label(f, text="Created by Gabbo", height="3", width="500",
+Label(f, text="Created by gEth0", height="3", width="500",
       font=("Francisco", 10), bg="grey").place(x="30", y="70")
 label = Label(f, bg="grey", font=("Francisco", 15, 'bold'))
 label.place(x="380", y="25")
@@ -43,9 +44,6 @@ def digital_clock():
     time_live = time.strftime("%H:%M")
     label.config(text=time_live)
     label.after(200, digital_clock)
-
-
-digital_clock()
 
 
 def butt():
@@ -119,47 +117,49 @@ def butt4():
         webbrowser.open(ing)
 
 
-Label(f, text="Lunedì", bg="grey", font=("Francisco")).place(x="70", y="80")
-Label(f, text="Martedì", bg="grey", font=("Francisco")).place(x="70", y="125")
-Label(f, text="Mercoledì", bg="grey", font=(
+digital_clock()
+Label(f, text="Monday", bg="grey", font=("Francisco")).place(x="70", y="80")
+Label(f, text="Tuesday", bg="grey", font=("Francisco")).place(x="70", y="125")
+Label(f, text="Wednesday", bg="grey", font=(
     "Francisco")).place(x="70", y="170")
-Label(f, text="Giovedì", bg="grey", font=("Francisco")).place(x="70", y="215")
-Label(f, text="Venerdì", bg="grey", font=("Francisco")).place(x="70", y="260")
-Label(f, text="Created by Gabbo", font=(
+Label(f, text="Thursday", bg="grey", font=("Francisco")).place(x="70", y="215")
+Label(f, text="Friday", bg="grey", font=("Francisco")).place(x="70", y="260")
+Label(f, text="Created by gEth0", font=(
     "Francisco", 10), bg="grey").place(x="390", y="370")
 stri = StringVar()
-stri.set("Scegli l'ora")
+stri.set("Choose the class")
 OptionMenu(f, stri, "1°Ora", "2°Ora", "3°Ora",
            "4°Ora", "5°Ora").place(x="200", y="80")
-Button(f, text="Apri Classroom", command=butt, font=("Francisco"),
+Button(f, text="Open", command=butt, font=("Francisco"),
        width="15", relief="sunken").place(x="320", y="80")
 stri1 = StringVar()
-stri1.set("Scegli l'ora")
+stri1.set("Choose the class")
 OptionMenu(f, stri1, "1°Ora", "2°Ora", "3°Ora",
            "4°Ora", "5°Ora").place(x="200", y="125")
-Button(f, text="Apri Classroom", command=butt1, font=("Francisco"),
+Button(f, text="Open", command=butt1, font=("Francisco"),
        width="15", relief="sunken").place(x="320", y="125")
 stri2 = StringVar()
-stri2.set("Scegli l'ora")
+stri2.set("Choose the class")
 OptionMenu(f, stri2, "1°Ora", "2°Ora", "3°Ora",
            "4°Ora", "5°Ora").place(x="200", y="170")
-Button(f, text="Apri Classroom", command=butt2, font=("Francisco"),
+Button(f, text="Open", command=butt2, font=("Francisco"),
        width="15", relief="sunken").place(x="320", y="170")
 stri3 = StringVar()
-stri3.set("Scegli l'ora")
+stri3.set("Choose the class")
 OptionMenu(f, stri3, "1°Ora", "2°Ora", "3°Ora",
            "4°Ora", "5°Ora").place(x="200", y="215")
-Button(f, text="Apri Classroom", command=butt3, font=("Francisco"),
+Button(f, text="Open", command=butt3, font=("Francisco"),
        width="15", relief="sunken").place(x="320", y="215")
 stri4 = StringVar()
-stri4.set("Scegli l'ora")
+stri4.set("Choose the class")
 OptionMenu(f, stri4, "1°Ora", "2°Ora", "3°Ora",
            "4°Ora", "5°Ora").place(x="200", y="260")
-Button(f, text="Apri Classroom", command=butt4, font=("Francisco"),
+Button(f, text="Open", command=butt4, font=("Francisco"),
        width="15", relief="sunken").place(x="320", y="260")
 
-Label(f, text="Created by Gabbo", height="3", width="500",
+Label(f, text="Created by gEth0", height="3", width="500",
       font=("Francisco", 10), bg="grey").place(x='300', y="300")
 
 
 f.mainloop()
+# gEth0
